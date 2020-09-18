88e98045062658825c8442011a116989024ad7d4
Danny393 <danielacosta393@gmail.com>
2020-09-18 16:12:32 -0400

    basic socket that connects to server with FTP protocol 21

--- myftp.py
+++ myftp.py
@@ -1,1 +1,13 @@
-print("Hello World")
+import socket
+
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverName = input("Please enter the host name of the server you want to connect to:\n")
+serverPort  = 21
+
+clientSocket.connect((serverName, serverPort))
+data = clientSocket.recv(2024)
+
+print("Connected to", serverName,"server with response", data)
+
+clientSocket.close()
