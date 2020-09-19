0f970aedc8e32f5e0eff30594d72891ea74b6926
Danny393 <danielacosta393@gmail.com>
2020-09-19 05:14:49 -0400

    cd works and fixed problem with ls

--- myftp.py
+++ myftp.py
@@ -1,10 +1,11 @@
 #import socket module to be able to create network sockets
 import socket
+import sys
 
 #establish basic client side socket
 clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#get server name from user and the port number for FTP connection is 21
+#get server name from user and the port number for FTP 21 through TCP connection
 serverName = input("Please enter the host name of the server you want to connect to:\n")
 serverPort  = 21
 
@@ -30,33 +31,33 @@ clientSocket.send(bytes("PASS " + password + "\r\n", 'ascii'))
 data = clientSocket.recv(1024)
 print(str(data)[2:-5])
 
-#read in the IP address and port of the host FTP socket connection
+#this is mainly done to get the IP Address of the client for TCP connections
 ipAddress, dataPort = clientSocket.getsockname()
 
-#this will split the IP address of the client
-start = 0
-end = 0
-i = 0
-h = []
-while(end < len(ipAddress)):
-    if(ipAddress[end] == '.'):
-        h.insert(i,ipAddress[start:end])
-        i = i + 1
-        start = end + 1
-    end = end + 1
-h.insert(i, ipAddress[start:])
+#this will provide a PORT command with a new port each time we use a TCP connection
+#this PORT command needs to be generated each time to make sure we are not using a busy port
+def getPortIPString(IP, freePort):
+    start = 0
+    end = 0
+    i = 0
+    h = []
+    while(end < len(IP)):
+        if(IP[end] == '.'):
+            h.insert(i,IP[start:end])
+            i = i + 1
+            start = end + 1
+        end = end + 1
+    h.insert(i, IP[start:])
 
-#this will encode the port number the client socket is using
-p2 = int(dataPort) % 256
-p1 = (int(dataPort) - p2) / 256
-p1 = int(p1)
+    #this will encode the port number the client socket is using
+    p2 = int(freePort) % 256
+    p1 = (int(freePort) - p2) / 256
+    p1 = int(p1)
 
-#premade PORT command to keep FTP loop clean
-portIPString = "PORT " + h[0] + ',' + h[1] + ',' + h[2] + ',' + h[3] + ',' + str(p1) + ',' + str(p2)
+    #premade PORT command to keep FTP loop clean
+    portCommand = "PORT " + h[0] + ',' + h[1] + ',' + h[2] + ',' + h[3] + ',' + str(p1) + ',' + str(p2)
 
-#socket made to aquire data from a server and client will act as a server when this socket is in use
-dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dataSocket.bind((ipAddress, dataPort))
+    return portCommand
 
 #actual FTP command loop
 loop = True
@@ -65,13 +66,34 @@ while loop:
 
     #case for quit, simple QUIT command
     if userInput == "quit":
+
         loop = False
         clientSocket.send(bytes("QUIT\r\n", "ascii"))
         data = clientSocket.recv(1024)
         print(str(data)[2:-5])
 
+    #case for cd, simple CWD command
+    if userInput[:2] == "cd":
+
+        clientSocket.send(bytes("CWD " + userInput[3:] + "\r\n", "ascii"))
+        data = clientSocket.recv(1024)
+        print(str(data)[2:-5])
+
     #case for ls, need to listen for connection using a server side socket running on client
     if userInput == "ls":
+
+        #socket made to aquire data from a server and client will act as a server when this socket is in use
+        dataSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        #sets data socket to free port
+        dataSocket.bind((ipAddress, 0))
+
+        #generated PORT command based on new data socket port
+        portIPString = getPortIPString(ipAddress, dataSocket.getsockname()[1])
+
+        #start listening for TCP connection request
+        dataSocket.listen(1)
+
         #tell server to prepare a TCP connection with client socket at client port and IP
         clientSocket.send(bytes(portIPString+"\r\n", "ascii"))
         data = clientSocket.recv(1024)
@@ -80,26 +102,27 @@ while loop:
         #NLST command that will require TCP connection and will tell server that the client is ready for data
         clientSocket.send(bytes("NLST\r\n", "ascii"))
 
-        #start listening for TCP connection
-        dataSocket.listen(1)
+        #TCP connection being accepted on client side from server for data transfer
+        print(ipAddress,dataPort, portIPString)
+        connectionSocket, address = dataSocket.accept()
 
         #TCP warning for incoming data
         data = clientSocket.recv(1024)
         print(str(data)[2:-5])
 
-        #TCP connection being accepted on client side from server for data transfer
-        connectionSocket, address = dataSocket.accept()
-
         #data receiving
-        data = connectionSocket.recv(1024)
+        list = connectionSocket.recv(1024)
         print("Receiving data from: " + str(address[0]) + "\n\n" +str(data.decode("ascii")))
 
         #TCP closing message
         data = clientSocket.recv(1024)
         print(str(data)[2:-5])
 
-        #close the TCP connection
+        print("bytes downloaded:", sys.getsizeof(list))
+
+        #close data and connection socket
+        connectionSocket.shutdown(socket.SHUT_RDWR)
         connectionSocket.close()
+        dataSocket.close()
 
 clientSocket.close()
-dataSocket.close()
